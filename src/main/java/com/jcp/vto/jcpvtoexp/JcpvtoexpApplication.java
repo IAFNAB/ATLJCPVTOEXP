3ab55e93fc1b20d5a3a6a4e47b903b55398e6a84
Tyler Howard <tyler.howard@howarditsolutions.com>
2026-08-20 18:13:14 -0400

    first commit

---
+++ src/main/java/com/jcp/vto/jcpvtoexp/JcpvtoexpApplication.java
@@ -0,0 +1,13 @@
+package com.jcp.vto.jcpvtoexp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcpvtoexpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JcpvtoexpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jcp/vto/jcpvtoexp/JcpvtoexpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcp.vto.jcpvtoexp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcpvtoexpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
